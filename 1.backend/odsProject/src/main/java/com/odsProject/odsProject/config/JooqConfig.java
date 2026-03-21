c395471ccd6e3128f1f26d1f24db05734af0f52a
mravenec <kcamposgo90@gmail.com>
2026-03-21 14:49:21 -0600

    Correccion de improtaciones en JooqConfig.java

--- 1.backend/odsProject/src/main/java/com/odsProject/odsProject/config/JooqConfig.java
+++ 1.backend/odsProject/src/main/java/com/odsProject/odsProject/config/JooqConfig.java
@@ -4,7 +4,6 @@ import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
 import org.jooq.SQLDialect;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
